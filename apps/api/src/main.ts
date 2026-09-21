2d949b26a8499fa2b1c4be23c69857157c4718fc
Taiwo Joseph <57369232+jtaiwo09@users.noreply.github.com>
2026-09-21 10:23:49 +0100

    fixed issuw with render waf firewall blocking raw html

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const express = require('express');
-
-  app.use(express.json({ limit: '10mb' }));
-  app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+  app.useBodyParser('json', { limit: '2mb' });
+  app.useBodyParser('urlencoded', { limit: '2mb', extended: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
